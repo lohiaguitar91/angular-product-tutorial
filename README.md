40af736725e57a81024cf84c186ef9e97f4919a8
lohiaguitar91 <kaushik@lohia.net>
2023-01-08 13:28:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-product-tutorial
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-a2b4qt)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('Disha do your work!');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
